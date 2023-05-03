7518f10e731c15db2d2d628fac9fbeb89f4348cc
loki0b <riqc0nt4ct@hotmail.com>
2023-05-03 18:02:25 -0300

    fix line

--- intro-computer-science/ai-project/snake-game-AI/agent.py
+++ intro-computer-science/ai-project/snake-game-AI/agent.py
@@ -84,7 +84,7 @@ class Agent:
         self.epsilon = 80 - self.number_games
         final_move = [0, 0, 0]
         if random.randint(0, 200) < self.epsilon:
-            move = random.randint(0, 2):
+            move = random.randint(0, 2)
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
